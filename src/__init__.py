a1ceac50e9e69154f7112f862f746208bd8a77b7
Elijah Castillo <elijah.castillo202@gmail.com>
2023-01-05 14:43:02 -0500

    Blueprints + Factory

--- src/__init__.py
+++ src/__init__.py
@@ -2,21 +2,28 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 
-#app instance
-app = Flask(__name__)
 
-#Config
-app.config["SECRET_KEY"] = "sadhfr2e9u8euhisuhd"  #For more security, use .env file
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+
 
 #database instance
-db = SQLAlchemy(app)
+db = SQLAlchemy()
 
 # Uncomment to create database
-# from src.models import PreRating, PostRating, WatchVideo
 # with app.app_context():
 #     db.create_all()
 
+def create_app():
+    #app instance
+    app = Flask(__name__)
 
-#import routes so flask can find them
-from src import routes
+    #Config
+    app.config["SECRET_KEY"] = "71WU6W2qf4srJSmxrl54ua1onxQHSMbF"  #For more security, use .env file
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+    
+    db.init_app(app)
+    
+    #Blueprints
+    from src.routes.main import routes
+    app.register_blueprint(routes)
+    
+    return app
