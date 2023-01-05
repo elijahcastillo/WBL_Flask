0eac4361d120fab206dbfa1fd1cc6be0ef7f5e6c
Elijah Castillo <elijah.castillo202@gmail.com>
2023-01-05 18:49:58 -0500

    Add Tests

--- src/__init__.py
+++ src/__init__.py
@@ -2,25 +2,24 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 
-
-
-
 #database instance
 db = SQLAlchemy()
 
-# Uncomment to create database
-# with app.app_context():
-#     db.create_all()
 
-def create_app():
+
+def create_app(uri="sqlite:///database.db"):
     #app instance
     app = Flask(__name__)
 
     #Config
     app.config["SECRET_KEY"] = "71WU6W2qf4srJSmxrl54ua1onxQHSMbF"  #For more security, use .env file
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+    app.config["SQLALCHEMY_DATABASE_URI"] = uri
     
     db.init_app(app)
+    
+    from src.models import PreRating, PostRating, WatchVideo
+    with app.app_context():
+        db.create_all()
     
     #Blueprints
     from src.routes.main import routes
